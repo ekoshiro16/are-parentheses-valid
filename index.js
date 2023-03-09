7aa8111d1b52fd1b469862321e65d053b76ed86f
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-08 17:17:11 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+As a new entry-level web developer, one of the hardest things you find with your code is that you often mess up with your matching opening and closing parentheses. There must be an algorithm out there that can analyze any snippet of code and determine whether the parentheses are validly matched.
+
+Write a function that takes a string, representing a series of opening and closing parnetheses, and outputs whether the order of the parentheses is valid. Return true if it is, false if it is invalid. 
+
+For example:
+
+If you were given this string:
+"()"
+Your function should output: true
+
+If you were given this string: 
+")(()))" 
+Your function should output: false
+
+If you were given this string:
+")"
+Your function should return: false
+
+If you were given this string:
+"((()()()))" 
+Your function should return: true
+*/
+
+// Write your code below: 
